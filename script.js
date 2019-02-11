86fdba7e596a7f781714d610d95854c9f887ca76
simonashabanova <43668759+simonashabanova@users.noreply.github.com>
2019-02-11 23:57:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function presmetaj(){
+    var tezina = document.getElementById('tezina').value;
+    var visina = document.getElementById('visina').value;
+
+    var presmetka = tezina / (visina * 2);
+    console.log(presmetka);
+}
